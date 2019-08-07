ce40f8b8b6d3632cea241c730fa14d8aa82198cf
antmas <psdsapienza@gmail.com>
2019-08-07 13:16:34 +0200

    Improve FF utilization

--- rtl/components/ovf_detect.sv
+++ rtl/components/ovf_detect.sv
@@ -19,23 +19,31 @@ module ovf_detect
 
 
   //
-  logic [31:0] past_counter; 
-  logic [31:0] past2_counter;
+  logic past_counter; 
+  logic past2_counter;
   logic ovfwdt_int;
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
-        past_counter  <= 32'b1;
+        past_counter  <= 1'b0;
+        past2_counter <= 1'b0;
     end else begin
-        past_counter  <= pres_counter_i;
-        past2_counter <= past_counter;
+        if (pres_counter_i == 32'hFFFF_FFFF) begin
+          past_counter  <= 1'b1;
+        end
+        if (pres_counter_i == 32'h0000_0000 && past_counter) begin
+          past2_counter <= 1'b1;
+        end
+        if (past_counter && past2_counter) begin
+          past_counter  <= 1'b0;
+          past2_counter <= 1'b0;
+        end
     end
   end
 
 
   always_comb begin
-    //todo... it is to big
-    if ( (past2_counter==32'hFFFF_FFFF && past_counter ==32'h0000_0000 ) || (past_counter==32'hFFFF_FFFF && pres_counter_i ==32'h0000_0000) ) begin
+    if ( past_counter && past2_counter ) begin
       ovfwdt_int = 1'b1;
     end else begin
       ovfwdt_int = 1'b0;
